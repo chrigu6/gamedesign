df7a36b6815d88e3c2595b85413f8c37fafb05f6
chrigu <chrigu6@gmx.ch>
2015-03-02 14:02:51 +0100

    initial

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	public Camera frontCamera;
+	public Camera aboveCamera;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(Input.GetKeyDown(KeyCode.Tab)){
+			frontCamera.enabled = !frontCamera.enabled;
+			aboveCamera.enabled = !frontCamera.enabled;
+		}
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		Vector3 movement = new Vector3 (moveHorizontal, 0f, 0f);
+		
+		rigidbody.AddForce (movement * speed * Time.deltaTime);
+	}
+}
