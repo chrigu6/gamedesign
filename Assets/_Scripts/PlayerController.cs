b70bd42a294097af14febe275a94b1ffd09ba739
chrigu <chrigu6@gmx.ch>
2015-03-03 08:56:47 +0100

    Changed jump key to space, added groundcheck

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -3,7 +3,10 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 	public float speed;
-	public float jumpSpeed;
+	public float jumpForce;
+
+	bool grounded = true;
+
 	public Camera frontCamera;
 	public Camera aboveCamera;
 
@@ -15,16 +18,39 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		if(Input.GetKeyDown(KeyCode.Tab)){
+
+		if (Input.GetKeyDown (KeyCode.Tab)) {
 			frontCamera.enabled = !frontCamera.enabled;
 			aboveCamera.enabled = !frontCamera.enabled;
 		}
 
 		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical") * jumpSpeed;
 
-		Vector3 movement = new Vector3 (moveHorizontal, moveVertical, 0f);
 
+		Vector3 movement = new Vector3 (moveHorizontal, 0f, 0f);
 		rigidbody.AddForce (movement * speed * Time.deltaTime);
+
+
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.Space) && grounded) {
+			rigidbody.AddForce(new Vector3(0, jumpForce, 0));
+		}
+	}
+
+	void OnCollisionEnter(Collision colide)
+	{
+		if (colide.gameObject.tag == "floor") {
+			grounded = true;
+		}
+	}
+
+	void OnCollisionExit(Collision colide)
+	{
+		if (colide.gameObject.tag == "floor") {
+			grounded = false;
+		}
 	}
 }
