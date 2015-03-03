b39f42ba824957fc5faa40b92e73b7a40d8f6470
chrigu <chrigu6@gmx.ch>
2015-03-03 11:04:18 +0100

    Changed player to a sphere, got rid of rotation, implementet possibility to change lane

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -10,6 +10,8 @@ public class PlayerController : MonoBehaviour {
 	public Camera frontCamera;
 	public Camera aboveCamera;
 
+	bool changeLane;
+
 	// Use this for initialization
 	void Start () {
 		frontCamera.enabled = true;
@@ -19,16 +21,30 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 
-		if (Input.GetKeyDown (KeyCode.Tab)) {
+		if (Input.GetKeyDown (KeyCode.Tab) && changeLane == false) {
 			frontCamera.enabled = !frontCamera.enabled;
 			aboveCamera.enabled = !frontCamera.enabled;
 		}
 
-		float moveHorizontal = Input.GetAxis ("Horizontal");
+		if (changeLane) {
+			rigidbody.constraints = RigidbodyConstraints.FreezePositionX|RigidbodyConstraints.FreezeRotationX|RigidbodyConstraints.FreezeRotationY ;
+			float moveVertical = Input.GetAxis ("Vertical");
+			Vector3 movement = new Vector3 (0, 0, moveVertical);
+			
+			rigidbody.AddForce (movement * speed * Time.deltaTime);
+		} else {
+			rigidbody.constraints = RigidbodyConstraints.FreezePositionZ|RigidbodyConstraints.FreezeRotationZ;
+			float moveHorizontal = Input.GetAxis ("Horizontal");
+			
+			
+			Vector3 movement = new Vector3 (moveHorizontal, 0f, 0f);
+			rigidbody.AddForce (movement * speed * Time.deltaTime);
+			
+		}
+		
 
 
-		Vector3 movement = new Vector3 (moveHorizontal, 0f, 0f);
-		rigidbody.AddForce (movement * speed * Time.deltaTime);
+
 
 
 	}
@@ -40,16 +56,27 @@ public class PlayerController : MonoBehaviour {
 		}
 	}
 
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "enemy") {
+			Destroy (other.gameObject);
+		}
+	}
+
 	void OnCollisionEnter(Collision colide)
 	{
-		if (colide.gameObject.tag == "floor") {
+		if (colide.gameObject.tag == "floor" || colide.gameObject.tag == "intersection") {
 			grounded = true;
+		}
+
+		if (colide.gameObject.tag == "intersection" && aboveCamera.enabled == true) {
+			changeLane = !changeLane;
 		}
 	}
 
 	void OnCollisionExit(Collision colide)
 	{
-		if (colide.gameObject.tag == "floor") {
+		if (colide.gameObject.tag == "floor" || colide.gameObject.tag == "intersection") {
 			grounded = false;
 		}
 	}
