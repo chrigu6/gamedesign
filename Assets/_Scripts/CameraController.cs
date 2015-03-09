43311e2c91b42ca7ec2c00e87145788b0b73191a
chrigu <chrigu6@gmx.ch>
2015-03-09 18:42:05 +0100

    added default layer to front-camera culling-mask

--- Assets/_Scripts/CameraController.cs
+++ Assets/_Scripts/CameraController.cs
@@ -24,7 +24,7 @@ public class CameraController : MonoBehaviour {
 		lane3Visible = false;
 		frontCamera.enabled = true;
 		aboveCamera.enabled = false;
-		frontCamera.cullingMask = ((1 << LayerMask.NameToLayer("1stLane") | 1 << LayerMask.NameToLayer("Default") | 1 << LayerMask.NameToLayer(activePlayer.tag)));
+		frontCamera.cullingMask = ((1 << LayerMask.NameToLayer("Default") | 1 << LayerMask.NameToLayer("1stLane") | 1 << LayerMask.NameToLayer("Default") | 1 << LayerMask.NameToLayer(activePlayer.tag)));
 		(frontCamera.GetComponent(typeof(AudioListener)) as AudioListener).enabled = true;
 		(aboveCamera.GetComponent(typeof(AudioListener)) as AudioListener).enabled = false;
 	}
@@ -32,19 +32,19 @@ public class CameraController : MonoBehaviour {
 
 	void FixedUpdate(){
 		if (activePlayer.transform.position.z < 9){
-			frontCamera.cullingMask = ((1 << LayerMask.NameToLayer("1stLane") | 0 << LayerMask.NameToLayer("2ndLane") | 1 << LayerMask.NameToLayer(activePlayer.tag)));
+			frontCamera.cullingMask = ((1 << LayerMask.NameToLayer("Default") | 1 << LayerMask.NameToLayer("1stLane") | 0 << LayerMask.NameToLayer("2ndLane") | 1 << LayerMask.NameToLayer(activePlayer.tag)));
 			lane1Visible = true;
 			lane2Visible = false;
 			lane3Visible = false;
 		}
 		if (activePlayer.transform.position.z > 9){
-			frontCamera.cullingMask = ((0 << LayerMask.NameToLayer("1stLane") | 1 << LayerMask.NameToLayer("2ndLane") | 1 << LayerMask.NameToLayer(activePlayer.tag)));
+			frontCamera.cullingMask = ((1 << LayerMask.NameToLayer("Default") | 0 << LayerMask.NameToLayer("1stLane") | 1 << LayerMask.NameToLayer("2ndLane") | 1 << LayerMask.NameToLayer(activePlayer.tag)));
 			lane1Visible = false;
 			lane2Visible = true;
 			lane3Visible = false;
 		}
 		if (activePlayer.transform.position.z > 19){
-			frontCamera.cullingMask = ((0 << LayerMask.NameToLayer("2ndLane") | 1 << LayerMask.NameToLayer("3rdLane") | 1 << LayerMask.NameToLayer(activePlayer.tag)));
+			frontCamera.cullingMask = ((1 << LayerMask.NameToLayer("Default") | 0 << LayerMask.NameToLayer("2ndLane") | 1 << LayerMask.NameToLayer("3rdLane") | 1 << LayerMask.NameToLayer(activePlayer.tag)));
 			lane1Visible = false;
 			lane2Visible = false;
 			lane3Visible = true;
