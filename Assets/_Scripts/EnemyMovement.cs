2fd4fbbc02822ecca57a7aff170d95453d9dc4d6
Faelg5 <raphael.tuor@gmail.com>
2015-03-12 17:31:05 +0100

    Added Nav Mesh Agent property to the ennemy so he follow the player

---
+++ Assets/_Scripts/EnemyMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+	Transform player1;
+	NavMeshAgent nav;
+	
+	void Awake (){
+		player1 = GameObject.FindGameObjectWithTag ("Player1").transform;
+		nav = GetComponent <NavMeshAgent> ();
+	}
+
+	void Update () {
+		nav.SetDestination (player1.position);
+	}
+}
