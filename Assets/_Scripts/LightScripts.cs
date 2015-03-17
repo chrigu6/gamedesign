6f56d771679d916c46a70f632d43bab238ea5788
chrigu <chrigu6@gmx.ch>
2015-03-17 09:30:56 +0100

    added background for above camer, added light effect

---
+++ Assets/_Scripts/LightScripts.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class LightScripts : MonoBehaviour {
+
+	public Light spotLight;
+	public float speed = 0.25f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 position = spotLight.transform.position + new Vector3 (speed, 0, 0);
+		spotLight.transform.position = position;
+
+		if (spotLight.transform.position.x > 60)
+			spotLight.transform.position = new Vector3(-10,spotLight.transform.position.y,spotLight.transform.position.z);
+	}
+}
