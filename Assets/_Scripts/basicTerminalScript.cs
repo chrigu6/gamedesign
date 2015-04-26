8dab1af16edcd635aed85065c2a9d836c3b2c0e6
chrigu <chrigu6@gmx.ch>
2015-04-26 15:14:53 +0200

    imroved terminal script

--- Assets/_Scripts/basicTerminalScript.cs
+++ Assets/_Scripts/basicTerminalScript.cs
@@ -1,87 +1,104 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
 using UnityEngine.UI;
 
 public class basicTerminalScript : MonoBehaviour {
 	
-		
-		private bool displayLabel = false;
-		public float letterPause = 0.2f;
-		public AudioClip sound;
-		public InputField inputText;
-		
-		public string message;
-		private char[] chars;
-		private int i = 0;
-		private AudioSource terminalAudio;
-		
-		// Use this for initialization
-		void Start () {
-			StartCoroutine(FlashLabel());
-		message = message.ToUpper ();
-			chars = message.ToCharArray ();
+	private bool isWriting = false;
+	private bool cursorVisible = false;
+	
+	public float cursorSpeed = 0.2f;
+	public float letterPause = 0.2f;
+	
+	public AudioClip sound;
+	
+	public string fileName;
+	private char[] chars;
+	private int i = 0;
+	private int lines = 0;
+	private AudioSource terminalAudio;
+	
+	// Use this for initialization
+	void Start () {
+		string message = File.ReadAllText (Application.dataPath+ "/" + fileName).ToUpper();
+		chars = message.ToCharArray();
 		terminalAudio = gameObject.GetComponent<AudioSource> ();
-		}
-		
-		
-		
-		IEnumerator FlashLabel() {
-			while (true) {
-				displayLabel = true;
-				yield return new WaitForSeconds(.5f);
-				displayLabel = false;
-				yield return new WaitForSeconds(.5f);
-			}
-		}
-		
-		public void update(){
-			Debug.Log (this.inputText.textComponent.text);
-			char[] a = this.inputText.textComponent.text.ToCharArray ();
-			this.gameObject.GetComponent<Text> ().text += chars[a.Length-1];
-			
-		}
-		
-		void OnGUI(){
-			
-			if (i < chars.Length) {
-				string a = this.gameObject.GetComponent<Text> ().text;
-				char[] test = a.ToCharArray();
-				if(test.Length == 0)
-				{
-					this.gameObject.GetComponent<Text> ().text += chars[i];
-					i++;
-					if (sound)
-					{
-					terminalAudio.clip = sound;
-					terminalAudio.Play();
-					}
-					
-				}
-				else{
-					if (!(test[test.Length-1] == '▇')){
-						this.gameObject.GetComponent<Text> ().text += chars[i];
-						i++;
-					if (sound && !terminalAudio.isPlaying)
-					{
-						terminalAudio.clip = sound;
-						terminalAudio.Play();
-					}
-					}
-				}
-			}
-			
-			if (displayLabel == true) {
-				string a = this.gameObject.GetComponent<Text> ().text;
-				char[] test = a.ToCharArray();
-				if (test[test.Length-1] == '▇'){
-					this.gameObject.GetComponent<Text> ().text = a.Substring(0,a.Length-1);
-				}
-			} else {
-				string a = this.gameObject.GetComponent<Text> ().text;
-				char[] test = a.ToCharArray();
-				if (!(test[test.Length-1] == '▇')){
-					this.gameObject.GetComponent<Text> ().text = this.gameObject.GetComponent<Text> ().text + "▇";
-				}
+	}
+	
+	void OnGUI() {
+		//If the end of the message is not reached and not allready writing startwriting
+		if (!isWriting && (i < chars.Length - 1)) {
+			isWriting = true;
+			cursorVisible = false;
+			StartCoroutine(TypeText());
+			StopCoroutine(ShowCursor());
+		} else {
+			if(!cursorVisible && (i > chars.Length - 1))
+			{
+				cursorVisible = true;
+				StopCoroutine(TypeText());
+				StartCoroutine(ShowCursor());
 			}
 		}
 	}
+	
+	IEnumerator TypeText ()
+	{
+		foreach (char letter in chars) {
+			
+			i++;
+			this.gameObject.GetComponent<Text> ().text += letter;
+			if (sound && !terminalAudio.isPlaying)
+			{
+				terminalAudio.clip = sound;
+				terminalAudio.Play ();
+			}
+
+
+			//If the textline is to long, start a new one
+			if(i%35 == 0 && !(i ==0))
+			{
+				this.gameObject.GetComponent<Text> ().text += '\n';
+			}
+			
+			//Count the number of lines if the number of lines reach the bottom of the textfield, start to wrap the lines
+			if (letter == '\n')
+			{
+				lines++;
+				if (lines > 5)
+				{
+					wrapLines();
+				}
+			}
+			
+			
+			yield return 0;
+			yield return new WaitForSeconds (letterPause);
+		}
+	}
+	
+	void wrapLines ()
+	{
+		string a = this.gameObject.GetComponent<Text> ().text;
+		int i = a.IndexOf ("\n");
+		this.gameObject.GetComponent<Text> ().text = a.Substring(i+1);
+		
+	}
+	
+	IEnumerator ShowCursor() {
+		while (true) {
+			string a = this.gameObject.GetComponent<Text> ().text;
+			char lastChar = a.ToCharArray () [a.Length-1];
+			
+			if (lastChar == '▇') {
+				this.gameObject.GetComponent<Text> ().text = a.Substring (0, a.Length - 1);
+			} else {
+				this.gameObject.GetComponent<Text> ().text = this.gameObject.GetComponent<Text> ().text + "▇";
+			}
+			
+			yield return 0;
+			yield return new WaitForSeconds (cursorSpeed);
+		}
+	}
+}
---
+++ Assets/_Scripts/test.cs
@@ -0,0 +1,133 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.IO;
+
+public class test : MonoBehaviour {
+	
+	
+	private bool displayLabel = false;
+	private bool displayCursor = false;
+	public float letterPause = 0.2f;
+	public float cursorPause = 0.5f;
+	public AudioClip sound;
+	public InputField inputText;
+	
+	public string fileName;
+	private char[] chars;
+	private int i = 0;
+	private int lines = 0;
+	private AudioSource terminalAudio;
+	
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(FlashLabel());
+		StartCoroutine(FlashCursor());
+		string message = File.ReadAllText (Application.dataPath+ "/" + fileName);
+		message = message.ToUpper ();
+		chars = message.ToCharArray ();
+		terminalAudio = gameObject.GetComponent<AudioSource> ();
+	}
+	
+	
+	
+	IEnumerator FlashLabel() {
+		while (true) {
+			displayLabel = true;
+			yield return new WaitForSeconds(letterPause);
+			displayLabel = false;
+			yield return new WaitForSeconds(letterPause);
+		}
+	}
+	
+	IEnumerator FlashCursor() {
+		while (true) {
+			displayCursor = true;
+			yield return new WaitForSeconds(cursorPause);
+			displayCursor = false;
+			yield return new WaitForSeconds(cursorPause);
+		}
+	}
+	
+	
+	public void update(){
+		Debug.Log (this.inputText.textComponent.text);
+		char[] a = this.inputText.textComponent.text.ToCharArray ();
+		this.gameObject.GetComponent<Text> ().text += chars[a.Length-1];
+		
+	}
+	
+	void OnGUI(){
+		if (displayLabel == true) {
+			if (i < chars.Length) {
+				string a = this.gameObject.GetComponent<Text> ().text;
+				char[] test = a.ToCharArray ();
+				if (test.Length == 0) {
+					this.gameObject.GetComponent<Text> ().text += chars [i];
+					i++;
+					if (sound) {
+						terminalAudio.clip = sound;
+						terminalAudio.Play ();
+					}
+					
+				} else {
+					if (!(test [test.Length - 1] == '▇')) {
+						if (lines > 17 && (chars [i] == '\n' || i % 35 == 0)) {
+							a = this.gameObject.GetComponent<Text> ().text;
+							string result = "";
+							char[] split = {'\n'};
+							string[] s = a.Split (split);
+							for (int j = 1; j<s.Length; j++) {
+								result += s [j];
+								
+								if (!(result [result.Length - 1] == '\n')) {
+									result += '\n';
+								}
+								
+								
+							}
+							
+							this.gameObject.GetComponent<Text> ().text = result;
+							
+							
+							
+						}
+						if (i % 35 == 0 && !(test [test.Length - 1] == '\n')) {
+							Debug.Log (test [test.Length - 1]);
+							lines ++;
+							this.gameObject.GetComponent<Text> ().text += "\n";
+						}
+						
+						if (chars [i] == '\n') {
+							if (lines > 17) {
+								i++;
+							}
+							lines++;
+						}
+						this.gameObject.GetComponent<Text> ().text += chars [i];
+						i++;
+						if (sound && !terminalAudio.isPlaying) {
+							terminalAudio.clip = sound;
+							terminalAudio.Play ();
+						}
+					}
+				}
+			}
+		}
+		
+		
+		if (displayCursor == true) {
+			string a = this.gameObject.GetComponent<Text> ().text;
+			char[] test = a.ToCharArray();
+			if (test[test.Length-1] == '▇'){
+				this.gameObject.GetComponent<Text> ().text = a.Substring(0,a.Length-1);
+			}
+		} else {
+			string a = this.gameObject.GetComponent<Text> ().text;
+			char[] test = a.ToCharArray();
+			if (!(test[test.Length-1] == '▇')){
+				this.gameObject.GetComponent<Text> ().text = this.gameObject.GetComponent<Text> ().text + "▇";
+			}
+		}
+	}
+}
