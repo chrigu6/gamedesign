ad85584e40e8a91d8bd0e3b91651bd2edfd7adec
Raphaël Tuor <raphael.tuor@gmail.com>
2015-05-22 14:58:42 +0200

    score

--- Assets/_Scripts/EnemyHealth.cs
+++ Assets/_Scripts/EnemyHealth.cs
@@ -7,18 +7,22 @@ public class EnemyHealth : MonoBehaviour {
 	public float sinkSpeed = 5.5f;
 	public int scoreValue = 10;
 	public AudioClip deathClip;
-
+	
 	AudioSource enemyAudio;
 	ParticleSystem hitParticles;
 	SphereCollider sphereCollider;
+	PlayerScore playerScore;
+	
 	public bool isDead;
 	bool isSinking;
-
+	
 	void Awake (){
 		enemyAudio = GetComponent <AudioSource> ();
 		hitParticles = GetComponent <ParticleSystem> ();
 		sphereCollider = GetComponent <SphereCollider> ();
 
+		playerScore = GameObject.Find("Player1").GetComponent<PlayerScore> ();
+		
 		currentHealth = startingHealth;
 	}
 	
@@ -28,7 +32,7 @@ public class EnemyHealth : MonoBehaviour {
 			transform.Translate (Vector3.up * sinkSpeed * Time.deltaTime);
 		}
 	}
-
+	
 	// Enemy takes damage
 	public void TakeDamage (int amount, Vector3 hitPoint){
 		if (isDead)
@@ -36,34 +40,36 @@ public class EnemyHealth : MonoBehaviour {
 		enemyAudio.Play ();
 		
 		currentHealth -= amount;
-
+		
 		// Move the particles to the hitposition (where enemy was hit)
 		hitParticles.transform.position = hitPoint;
 		hitParticles.Play ();
-
+		
 		// Player is dead
 		if (currentHealth <= 0 && !isDead) {
 			Death ();
 		}
 		
 	}
-
+	
 	void Death(){
 		isDead = true;
 		
 		sphereCollider.isTrigger = true;
-
+		
+		playerScore.AddScore (scoreValue);
+		
 		enemyAudio.clip = deathClip;
 		enemyAudio.Play ();
-
+		
 		StartSinking ();
 	}
-
+	
 	public void StartSinking (){
 		// turn off navmesh component only
 		GetComponent <NavMeshAgent> ().enabled = false;
 		GetComponent <Rigidbody> ().isKinematic = true;
-
+		
 		isSinking = true;
 		// Destroy the object after 2 seconds
 		Destroy (gameObject, 10f);
---
+++ Assets/_Scripts/PlayerScore.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerScore : MonoBehaviour {
+	public int startingScore;
+	int currentScore;
+	
+	int amount = 10;
+	bool scoreAdded = false;
+	string scoreText = "Score: 0";
+	
+	Text guiScore;
+	
+	
+	
+	// Use this for initialization
+	void Start () {
+		currentScore = startingScore;
+		guiScore = GameObject.Find("Score").GetComponent<Text> ();
+		guiScore.text = scoreText;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	
+	
+	public void AddScore(int amount)
+	{
+		scoreAdded = true;
+		currentScore += amount;
+		scoreText = "Score: " + currentScore.ToString ();
+		guiScore.text = scoreText;
+		//			playerAudio.Play();
+	}
+	
+	
+}
