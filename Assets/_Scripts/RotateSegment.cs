5f0f5a928f4a80d8ff87d99f836658e1da85d782
chrigu6 <chrigu6@gmx.ch>
2015-05-31 17:31:30 +0200

    added rotating platform

---
+++ Assets/_Scripts/RotateSegment.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateSegment : MonoBehaviour {
+	public float angle = 0.1f;
+	public Vector3 pivot;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.RotateAround (this.pivot, Vector3.up, this.angle);
+	}
+}
