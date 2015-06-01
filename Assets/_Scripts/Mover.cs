5b0061346b6ed2b8be91503ba1ca7911c4214745
chrigu6 <chrigu6@gmx.ch>
2015-06-01 12:09:01 +0200

    Shots can no longer pass trough object, they vanish after a certain distance

--- Assets/_Scripts/Mover.cs
+++ Assets/_Scripts/Mover.cs
@@ -4,9 +4,57 @@ using System.Collections;
 public class Mover : MonoBehaviour 
 {
 	public float speed;
+	private GameObject player1;
+	private GameObject player2;
+	float range = 0;
+
 
 	void Start ()
 	{
 		GetComponent<Rigidbody>().velocity = transform.right * speed;
+		player1 = GameObject.FindGameObjectWithTag ("Player1");
+		player2 = GameObject.FindGameObjectWithTag ("Player2");
+		if (player1 != null) {
+			range = player1.GetComponent<PlayerController>().range;
+		}
+		if (range == 0 && player2 != null) {
+			range = player2.GetComponent<PlayerController>().range;
+		}
 	}
+
+	void Update()
+	{
+		if (player1 != null) {
+			if (player1.GetComponent<PlayerController> ().isPlayerActive ()) {
+				if (Vector3.Distance (player1.transform.position, this.transform.position) > this.range) {
+					Destroy (this.gameObject);
+				}
+			}
+		}
+
+		if (player2 != null) {
+			if (player2.GetComponent<PlayerController> ().isPlayerActive ()) {
+				if (Vector3.Distance (player2.transform.position, this.transform.position) > this.range) {
+					Destroy (this.gameObject);
+				}
+			}
+		}
+	}
+
+
+	void OnCollisionEnter(Collision collide)
+	{
+		if (!collide.gameObject.name.Contains ("Player")) {
+			Destroy (this.gameObject);
+		}
+	}
+
+	void OnCollisioneExit(Collision collide)
+	{
+		if (!collide.gameObject.name.Contains ("Player")) {
+			Destroy (this.gameObject);
+		}
+	}
+
+
 }
