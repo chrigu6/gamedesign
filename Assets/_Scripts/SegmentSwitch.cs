e10dea755f32aa3e8b2a74155cadb734ab3911f1
chrigu6 <chrigu6@gmx.ch>
2015-06-02 15:16:20 +0200

    added segmentswitch

---
+++ Assets/_Scripts/SegmentSwitch.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SegmentSwitch : MonoBehaviour {
+
+	public Material activatedMat;
+	public Material deactivatedMat;
+	public Transform objectToMove;
+	private Vector3 startPoint;
+	public Vector3 endPoint;
+	public bool canMoveTwice;
+	public float speed;
+
+	private int running = 0;
+	private bool activated = false;
+	private GameObject player;
+	private AudioSource audio;
+
+
+	void Awake() 
+	{
+		this.startPoint = this.objectToMove.position;
+		player = GameObject.Find ("Cameras").GetComponent<CameraController> ().activePlayer;
+		audio = GetComponent<AudioSource> ();
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		if (other.gameObject == player) 
+		{
+			if(Input.GetButton("Action") && (!this.activated || this.canMoveTwice) && running < 1)
+			{
+				this.running++;
+				activated = !activated;
+				StartCoroutine(moveSegment(speed));
+				Renderer switchButton = GetComponent<Renderer> ();
+				if(activated)
+				{
+					switchButton.material = activatedMat;
+				}
+				else{
+					switchButton.material = deactivatedMat;
+				}
+				audio.Play ();
+			}
+		}
+	}
+
+	IEnumerator moveSegment(float time)
+	{
+		float i = 0.0f;
+		float rate = 1.0f / time;
+
+		if (!canMoveTwice) {
+			while (i < 1.0f) {
+				i += Time.deltaTime * rate;
+				this.objectToMove.transform.localPosition = Vector3.Lerp(this.startPoint, this.endPoint, i);
+				this.running--;
+				yield return null;
+			}
+		} else {
+			Vector3 currentPosition = this.objectToMove.position;
+			if(activated){
+				while (i < 1.0f) {
+					i += Time.deltaTime * rate;
+					this.objectToMove.transform.localPosition = Vector3.Lerp(currentPosition, this.endPoint, i);
+					this.running--;
+					yield return null;
+				}
+			}
+			else{
+				while (i < 1.0f) {
+					i += Time.deltaTime * rate;
+					this.objectToMove.localPosition = Vector3.Lerp(currentPosition, this.startPoint, i);
+					this.running--;
+					yield return null;
+				}
+			}
+		}
+	}
+}
